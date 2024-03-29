48eaa80e8419efe516933035af3ecbc34daa1e58
robertsstr <149763082+robertsstr@users.noreply.github.com>
2024-03-29 16:41:56 +0300

    Add project files.

---
+++ CvManagerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CvManagerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
